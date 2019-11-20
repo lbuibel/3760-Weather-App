93caaaffc15020b01666803eb099cba185208cf8
Logan Uibel <lbuibel@gmail.com>
2019-11-20 09:19:52 -0700

    added high and low temp with weather icon image

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-let apiKey = 'a9da6dd423feb80b529614e938d9224a'
+// let apiKey = 'a9da6dd423feb80b529614e938d9224a'
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,35 +11,12 @@ app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
     res.render('index', {weather: null, error: null});
-
-    // let city = req.body.city;
-    // let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-    
-    // request(url, function(err, response, body){
-    //     if(err){
-    //         res.render('index', {weather: null, error: 'Error, please try again'});
-    //     } else {
-    //         let weather_json = JSON.parse(body)
-    //         if(weather.main == undefined){
-    //         res.render('index', {weather: null, error: 'Error, please try again'});
-    //     } else {
-    //         var weather = {
-    //             city : city,
-    //             temp : Math.round(weather_json.main.temp),
-    //             description : weather_json.weather.description,
-    //             icon : weather_json.weather.icon,
-    //         }
-    //         var weather_data = {weather : weather};
-    //         res.render('index', {weather: weather_data, error: null})
-    //     }
-    // }
-    // })
 })
 
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=a9da6dd423feb80b529614e938d9224a`
 
   request(url, function (err, response, body) {
     if(err){
@@ -50,10 +27,15 @@ app.post('/', function (req, res) {
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
         console.log(weather)
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}`;
-        let high = `${weather.main.temp_max}`
+        let weatherText = `${Math.round(weather.main.temp)} degrees in ${weather.name}`;
+        let city = `${weather.name}`
+        let high = `${Math.round(weather.main.temp_max)}`
+        let low = `${Math.round(weather.main.temp_min)}`
+        let icon = weather.weather[0].icon
         res.render('index', {weather: weatherText,
                             high: high, 
+                            low: low,
+                            icon: icon,
                             error: null});
       }
     }
